28c043355c84cf2d469f41821dccea9ddd48ae69
rchan9889 <164683407+rchan9889@users.noreply.github.com>
2024-10-06 16:52:35 -0400

    Added Character and Bullet placeholders
    programmed basic movement for character and bullet
    bullet deletes player on contact

---
+++ bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var distance_travelled = 0;
+
+func _physics_process(delta):
+	const SPEED = 1000
+	const RANGE = 1200
+	
+	var direction = Vector2.LEFT.rotated(rotation)
+	position += direction * 1000.0 * delta
+	
+	distance_travelled += SPEED * delta
+	if(distance_travelled > RANGE):
+		queue_free()
+
+
+func _on_body_entered(body):
+	if body.has_method("bullet_hit"):
+		body.bullet_hit()
