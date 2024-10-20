1bc0830db6dea5af4c5d76872b910535d16630e9
rchan9889 <164683407+rchan9889@users.noreply.github.com>
2024-10-20 18:45:54 -0400

    attack hitbox and launcher stuff

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,12 +1,25 @@
 extends Node
+class_name GameManager
+
+static var global: GameManager
 
 @export var world: Node2D
 @export var player_prefab: PackedScene
 @export var spawn_positions: Node2D
+@export var timer: Label
+@export var minigame_manager: MiniGameManager
+@export var time: float = 180
+
+var player_kills_dict = {}
+var player_deaths_dict = {}
 
 func _ready():
-	MiniGameManager.game_started.connect(on_game_started)
-	
+	minigame_manager.game_started.connect(on_game_started)
+	if global != null:
+		queue_free()
+		return
+	global = self
+
 func on_game_started(player_data_array: Array):
 	print(JSON.stringify(player_data_array))
 	for i in range(len(player_data_array)):
@@ -15,3 +28,32 @@ func on_game_started(player_data_array: Array):
 		world.add_child(player_instance)
 		player_instance.construct(player_data)
 		player_instance.position = spawn_positions.get_child(i).position
+		player_kills_dict[i] = 0
+		player_deaths_dict[i] = 0
+		player_instance.death.connect(on_player_death.bind(i))
+
+func on_player_death(attacker_index: int, victim: int):
+	player_kills_dict[attacker_index] += 1
+	player_deaths_dict[victim] += 1
+
+func _process(delta: float) -> void:
+	time -= delta
+	timer.text = str(ceil(time))
+	if time <= 0:
+		var points = []
+		for player_index in player_kills_dict:
+			points.append({
+				"points": player_kills_dict[player_index] - player_deaths_dict[player_index],
+				"player": player_index
+			})
+		points.sort_custom(func(a, b): b.points - a.points)
+		var rewards = [3, 2, 1]
+		var results = []
+		for i in range(len(points)):
+			if i >= len(rewards):
+				break
+			results.append[{
+				"points": rewards[i],
+				"player": points[i].player
+			}]
+		minigame_manager.end_game(results)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 class_name Player
 
+signal death(attacker_index: int)
+
 @export var hat_sprite: Sprite2D
 @export var joy_device_id: int = -1
 @export var deadzone: float = 0.2
 @export var attack_pivot: Node2D
 @export var enable_attack_hitbox: Area2D
-var attack_direction: Vector2
 @export var health: int = 3
+@export var attack_cd: float = 2
+
+var attack_direction: Vector2
+var attack_press: bool
+var on_cooldown: bool = false
+var player_data: MiniGameManager.PlayerData
 
 func construct(player_data: MiniGameManager.PlayerData):
+	self.player_data = player_data
 	hat_sprite.modulate = player_data.color
 	if len(Input.get_connected_joypads()) >= player_data.number:
 		joy_device_id = Input.get_connected_joypads()[player_data.index]
@@ -35,22 +43,28 @@ func _physics_process(delta):
 		get_node("CharacterPlaceholder_png").flip_h = true
 	move_and_slide()
 	
-	if Input.is_joy_button_pressed(joy_device_id, JOY_BUTTON_RIGHT_SHOULDER):
+	var new_attack_press = Input.is_joy_button_pressed(joy_device_id, JOY_BUTTON_RIGHT_SHOULDER)
+	if new_attack_press && new_attack_press != attack_press && !on_cooldown:
 		attack()
+	attack_press = new_attack_press
 	
 func attack():
+	on_cooldown = true
 	enable_attack_hitbox.monitoring = true
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().physics_frame
 	enable_attack_hitbox.monitoring = false
+	await get_tree().create_timer(2).timeout
+	on_cooldown = false
 
 func bullet_hit():
 	queue_free()
 
-func player_hit():
+func player_hit(attacker_index: int):
 	health -= 1
 	if health <= 0:
+		death.emit(attacker_index)
 		queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	var other_player: Player = body
-	other_player.player_hit()
+	other_player.player_hit(player_data.index)
