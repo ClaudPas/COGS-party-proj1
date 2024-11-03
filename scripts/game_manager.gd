ad80203038949ea8d357c52c6eada535de11ca7a
Claudgy Pascal <claudgy@gmail.com>
2024-11-03 16:02:24 -0500

    Bullets are spawning, need to fix collisions

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,6 +3,10 @@ extends Node
 @export var world: Node2D
 @export var player_prefab: PackedScene
 @export var spawn_positions: Node2D
+@export var bullet: PackedScene
+@export var path_follow: PathFollow2D
+
+
 
 func _ready():
 	MiniGameManager.game_started.connect(on_game_started)
@@ -15,3 +19,32 @@ func on_game_started(player_data_array: Array):
 		world.add_child(player_instance)
 		player_instance.construct(player_data)
 		player_instance.position = spawn_positions.get_child(i).position
+		
+	
+
+
+func _on_mob_timer_timeout():
+	var bullet = load("res://scenes/bullet.tscn")
+# Create a new instance of the Mob scene.
+	var mob = bullet.instantiate()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randi()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(700, 1500), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
--- scripts/player.gd
+++ scripts/player.gd
@@ -10,7 +10,15 @@ func construct(player_data: MiniGameManager.PlayerData):
 	if len(Input.get_connected_joypads()) >= player_data.number:
 		joy_device_id = Input.get_connected_joypads()[player_data.index]
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 	if joy_device_id < 0:
 		return
 	var direction = Vector2(Input.get_joy_axis(joy_device_id, JOY_AXIS_LEFT_X), Input.get_joy_axis(joy_device_id, JOY_AXIS_LEFT_Y))
@@ -31,5 +39,6 @@ func _unhandled_input(event):
 		if event.pressed and event.keycode == KEY_SPACE:
 			get_node("CharacterPlaceholder_png").flip_v = false
 	
-func bullet_hit():
+func bullet_hit(_body):
+	emit_signal("hit")
 	queue_free()
