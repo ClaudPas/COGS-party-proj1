38b76012e2650fd6003e8f79801ca9848437f046
Claudgy Pascal <claudgy@gmail.com>
2024-11-24 17:14:22 -0500

    updated ver

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,11 @@ extends RigidBody2D
 class_name Bullet
 
 @export var min_speed = 150.0
+<<<<<<< Updated upstream
 @export var max_speed = 250.0
+=======
+@export var max_speed = 200.0
+>>>>>>> Stashed changes
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,9 +3,17 @@ extends Node
 @export var world: Node2D
 @export var player_prefab: PackedScene
 @export var spawn_positions: Node2D
+<<<<<<< Updated upstream
 @export var bullet: PackedScene
 @export var path_follow: PathFollow2D
 
+=======
+@export var timer: Label
+@export var minigame_manager: MiniGameManager
+@export var time: float = 180
+@export var bullet: PackedScene
+@export var path_follow: PathFollow2D
+>>>>>>> Stashed changes
 
 
 func _ready():
@@ -28,6 +36,25 @@ func _on_mob_timer_timeout():
 # Create a new instance of the Mob scene.
 	var mob = bullet.instantiate()
 
+<<<<<<< Updated upstream
+=======
+func spawn_player(player_index: int):
+	var player_data = player_data_arr[player_index]
+	var player_instance: Player = player_prefab.instantiate()
+	world.add_child(player_instance)
+	player_instance.construct(player_data)
+	player_instance.position = spawn_positions.get_child(player_index).position
+	player_kills_dict[player_index] = 0
+	player_deaths_dict[player_index] = 0
+	player_instance.death.connect(on_player_death.bind(player_index))
+
+
+func _on_mob_timer_timeout():
+	var bullet = load("res://scenes/bullet_obj.tscn")
+# Create a new instance of the Mob scene.
+	var mob = bullet.instantiate()
+
+>>>>>>> Stashed changes
 	# Choose a random location on Path2D.
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randi()
@@ -43,7 +70,11 @@ func _on_mob_timer_timeout():
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
+<<<<<<< Updated upstream
 	var velocity = Vector2(randf_range(700, 1500), 0.0)
+=======
+	var velocity = Vector2(randf_range(150, 750), 0.0)
+>>>>>>> Stashed changes
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
